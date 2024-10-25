de0645bf2766e3d53c624be669a03ba9ea3d125c
zer0warm <dqhieu.2016@gmail.com>
2024-10-25 18:43:17 +0700

    Use a subdirectory venv
    - create venv if neccessary
    - install requirements.txt if just created venv
    - run program with venv's binary

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
-bin/python fetch.py
-bin/python visualize.py
+test -d venv || {
+	echo "creating venv..."
+	(set -x; python3 -m venv venv)
+	echo "installing required packages..."
+	(set -x; venv/bin/pip install -r requirements.txt)
+}
+
+venv/bin/python fetch.py
+venv/bin/python visualize.py
+
 open output.html
